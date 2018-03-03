43a7e11d165c5f4425995a9724d999f0dae92a5f
Michael Pope <popemray1986@gmail.com>
2018-03-03 20:51:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,6 +34,8 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
 
+    DB[:conn].execute("SELECT last_insert_rowid() FROM students")
+
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
